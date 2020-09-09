d5af2e7b7aa059266341b161b559e3049929ec93
BosenChang <70292242+BosenChang@users.noreply.github.com>
2020-09-08 19:34:59 -0700

    Update short.js

--- short.js
+++ short.js
@@ -1,1 +1,8 @@
-alert("hi")
+if (confirm("???") == true) then {
+alert("Haha pranked you")
+var ??? = true
+} else {
+alert("You passed the test! :)")
+var ??. = false
+}
+alert("Answer is: " + true)
